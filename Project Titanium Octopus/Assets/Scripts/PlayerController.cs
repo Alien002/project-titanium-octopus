92c00dc1596eb100da3776795931ad8bd3bb6d27
Elijah Nicasio <ElijahN8@gmail.com>
2019-04-14 22:32:33 -0700

    Added testing room and prototype player model. Player can move with wasd.

---
+++ Project Titanium Octopus/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = .2f;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        //rb.AddForce(movement * speed);
+        transform.Translate(movement * speed);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Wall"))  // or if(gameObject.CompareTag("YourWallTag"))
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
