eda94190442155a4662aad8b7a9fd1fc71bd6405
Alic Lien <alic.1125@gmail.com>
2019-05-22 23:34:59 -0700

    Added Enemy/Enemy_core models prefab and HP scripts, added player HP script, added bullet dmg script

---
+++ Project Titanium Octopus/Assets/Scripts/EnemyCore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int coreMaxHealth;
+    public int coreCurrHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coreCurrHealth = coreMaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coreCurrHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void KillCore()
+    {
+        coreCurrHealth = 0;
+    }
+
+    public void SetMaxEnemyCore()
+    {
+        coreCurrHealth = coreMaxHealth;
+    }
+}
---
+++ Project Titanium Octopus/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int playerMaxHealth;
+    public int playerCurrHealth;
+    private GameObject gameDirector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCurrHealth = playerMaxHealth;
+        gameDirector = GameObject.Find("GameDirector");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerMaxHealth <= 0)
+        {
+            gameDirector.GetComponent<GameDirector>().restartLevel();   // resets level on death
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void DamagePlayer(int damage)
+    {
+        playerCurrHealth -= damage;
+    }
+}
